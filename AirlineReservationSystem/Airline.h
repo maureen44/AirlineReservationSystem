5dd2843519065dd704b7ce07c75f1cb2be768067
Maureen Kariuki <kmaureen44@gmail.com>
2018-09-12 12:32:11 -0700

    Add project files.

---
+++ AirlineReservationSystem/Airline.h
@@ -0,0 +1,72 @@
+#pragma once
+#include <string>
+
+namespace AirlineReservationSystem {
+
+	class Airline {
+
+	public:
+		Airline();
+		Airline(const std::string& menu, const std::string& schedule);
+
+		int getSeatNumber() const;
+		void setSeatNumber(int seatNumber);
+
+		const std::string& getMenu() const;
+		void setMenu(const std::string& menu);
+
+		bool isReserved() const;
+
+		const std::string& getSchedule() const;
+		void setSchedule(const std::string& schedule);
+
+		const std::string& getUserTicketInfo() const;
+		void setUserTicketInfo(const std::string& userInfo);
+
+		const std::string& getFirstName() const;
+		void setFirstName(const std::string& firstName);
+
+		const std::string& getLastName() const;
+		void setLastName(const std::string& lastName);
+
+		const std::string& getDepartureLocation() const;
+		void setDepatureLocation(const std::string& departureLocation);
+
+		const std::string& getArrivalLocation() const;
+		void setArrivalLocation(const std::string& arrivalLocation);
+
+		//const getDateOfBirth() const;
+		//void setDateOfBirth();
+
+		//const getDepartureTime() const;
+		//void setDepartureTime();
+
+		//const getArrivalTime() const;
+		//void setArrivalTime();
+
+		void reserved();
+		void notReserved();
+		void userTicketInformation(const std::string& firstName, const std::string& lastName, 
+			const std::string& departureLocation, const std::string& arrivalLocation
+			);
+		void displayPassagerInfo() const;
+
+	private:
+		std::string mFirstName;
+		std::string mLastName;
+		std::string fMenu;
+		std::string fSchedule;
+		bool isReserved = false;
+		int fSeatNumber = -1;
+
+
+	};
+}
+
+//Application will present a menu system of choice to the user
+//Reserve a seat
+//Flight Schedule
+//Display passenger info
+//Flight details
+//User ticket infomation
+//Exit the program
