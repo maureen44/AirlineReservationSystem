a2a4d9c62124c9ab4f3213ff259384b27dbc0b20
bethanyzigler <34946388+bethanyzigler@users.noreply.github.com>
2018-09-17 21:50:47 -0700

    Deleted old portion of PassengerDB.h

--- AirlineReservationSystem/PassengerDB.h
+++ AirlineReservationSystem/PassengerDB.h
@@ -15,17 +15,10 @@ namespace AirlineReservationSystem {
 	class PassengerDB
 	{
 	public:
-<<<<<<< HEAD
-		UserInfo& addPassenger(const string& firstName,
-			const string& lastName, const string& passportNo, const string& gender, const string& nationality); 
-		UserInfo& getPassenger(int bookingNo);
-		UserInfo& getPassenger(const std::string& firstName,
-=======
 		Airline& addPassenger(const std::string& firstName,
 			const std::string& lastName); //TODO: add DOB, passport no.? 
 		Airline& getPassenger(int bookingNo);
 		Airline& getPassenger(const std::string& firstName,
->>>>>>> 88d18be9357448245339a57aeb3ea0798e866b44
 			const std::string& lastName);
 
 		void displayPassengerInfo() const;
