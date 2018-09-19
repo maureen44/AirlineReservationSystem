7c63d9bedaee40de5b92454968daa95abc014501
Maureen Kariuki <kmaureen44@gmail.com>
2018-09-18 23:18:19 -0700

    Corrected errors on Airline.cpp and Airline.h Added code to get passengers on AirlineDB.cpp and AirlineDB.h Added method on the main page Got all the Databases into one database.

--- AirlineReservationSystem/Airline.cpp
+++ AirlineReservationSystem/Airline.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include <iostream>
+#include <iomanip>
+#include <vector>
 #include "Airline.h"
 
 using namespace std;
@@ -8,12 +10,18 @@ namespace AirlineReservationSystem {
 
 	Airline::Airline(const std::string& departureLocation, const std::string& departureDate,
 		const std::string& departureTime, const std::string& arrivalLocation,
-		const std::string& arrivalDate, const std::string& arrivalTime,
-		int airlineNo)
+		const std::string& arrivalDate, const std::string& arrivalTime)
 		: mDepartureLocation(departureLocation), mAirlineDepartureDate(departureDate),
 		mDepartureTime(departureTime), mArrivalLocation(arrivalLocation), mAirlineArrivalDate(arrivalDate),
-		mArrivalTime(arrivalTime), mAirlineNumber(airlineNo) {
+		mArrivalTime(arrivalTime) {
 
+	}
+	const std::string& Airline::getAirlinePassenger(const std::string& firstName, const std::string& lastName,
+		const std::string& dateOfBirth, const std::string& passportNo,
+		const std::string& gender, const std::string& nationality) {
+		return
+			mFirstName, mLastName, mDateOfBirth,
+			mPassportNo, mGender, mNationality;
 	}
 
 	int Airline::getAirlineNo() const {
@@ -75,19 +83,33 @@ namespace AirlineReservationSystem {
 		mArrivalTime = arrivalTime;
 	}
 
-	void Airline::reserved() {
-		mReserved = true;
-
+	int Airline::getAirlinePrice() const {
+		return mAirlinePrice;
 	}
-	void Airline::notReserved() {
-		mReserved = false;	
+
+	void Airline::setAirlinePrice(int newAirlinePrice) {
+		mAirlinePrice = newAirlinePrice;
+	}
+
+	void Airline::reserve() {
+		mReserved = true;
+	}
+	void Airline::cancel() {
+		mReserved = false;
+	}
+
+	void Airline::reservedFirstClass(int firstClassIncreaceAmount) {
+		setAirlinePrice(getAirlinePrice() + firstClassIncreaceAmount);
+	}
+	void Airline::reservedEconomyClass() {
+		setAirlinePrice(getAirlinePrice());
 	}
 
 	bool Airline::isReserved() const {
 		return mReserved;
 
 	}
-	
+
 	int Airline::getSeatNumber() const {
 		return mSeatNumber;
 	}
@@ -95,15 +117,7 @@ namespace AirlineReservationSystem {
 		mSeatNumber = seatNo;
 	}
 
-	//Passenger Info
-
-	/*Airline::UserInfo(const std::string& firstName, const std::string& lastName,
-		const std::string& dateOfBirth, const std::string& passportNo,
-		const std::string& gender, const std::string& nationality) : mFirstName(firstName), mLastName(lastName),
-		mDateOfBirth(dateOfBirth), mPassportNo(passportNo), mGender(gender), mNationality(nationality) {
-
-	}*/
-
+	//Passenger Info	
 	const std::string& Airline::getFirstName() const {
 		return mFirstName;
 	}
@@ -152,30 +166,6 @@ namespace AirlineReservationSystem {
 		mBookingNumber = bookingNo;
 	}
 
-	bool Airline::isPassengerBooked() const {
-		return mBooked;
-	}
-
-	void Airline::book() {
-		mBooked = true;
-	}
-	void Airline::cancel() {
-		mBooked = false;
-	}
-
-	/*const std::string& getPassportExpiration() const;
-	void setPassportExpiration()*/;
-
-	void Airline::userTicketInformation(const std::string& firstName, const std::string& lastName) {
-
-	}
-	void Airline::userTicketInformation(int AirlineNumber, const std::string& departureLocation,
-		const std::string& depatureDate, const std::string& departureTime,
-		const std::string& arrivalLocation, const std::string& arrivalDate,
-		const std::string& arrivalTime) {
-
-	}
-
 	void Airline::displayAirline() const {
 
 		cout << "Airline " << getAirlineNo();
@@ -187,12 +177,110 @@ namespace AirlineReservationSystem {
 		cout << "Arrival Time " << getArrivalTime();
 	}
 
-	void Airline::displayPassagerInfo() const {
-		cout << "Passanger " << getFirstName() << ", " << getLastName() << endl;
+	void Airline::displayPassengerInfo() const {
+		cout << "Passenger " << getFirstName() << ", " << getLastName() << endl;
 		cout << "-------------------------------------------------------" << endl;
+		cout << (isReserved() ? "First Class " : "Economy Class ") << endl;
+		cout << "Booking Number " << getBookingNumber() << endl;
+		cout << "Airline Price " << getAirlinePrice() << endl;
 		cout << "Date of Birth " << getDateOfBirth() << endl;
 		cout << "Gender " << getGender() << endl;
 		cout << "Nationality " << getNationality() << endl;
 		cout << "Passport Number " << getPassportNo() << endl;
 	}
+
+
+	//void Airline::airlineSeats() const {
+	//	//int price(int row, int col); // calculate ticket price
+	//	bool isValid(int row, int seat);// is the seat at(row, seat) empty?
+	//	char seats[NUM_ROWS + 1][NUM_COLS + 1];
+	//	for (size_t row{ 1 }; row < NUM_ROWS + 1; ++row) {
+	//		for (size_t col{ 1 }; col < NUM_COLS + 1; ++col) {
+	//			seats[row][col] = EMPTY;
+	//		}
+	//	}
+	//	displaySeatingChart(seats);
+	//	int row = 0;
+	//	char seat = 'A';
+	//	char anotherSeat = 'y';
+	//	vector<int> rowsWanted;
+	//	vector<int> seatsWanted;
+	//	while (anotherSeat == 'y') {
+
+	//		cout << "What seat would you like? (row 1-15, seat 1-30) ";
+
+	//		cin >> row >> seat;
+
+	//		if (!isValid(row, seat)) {
+	//			cout << "Please enter: row 1-60, seat A-E " << endl;
+	//		}
+	//		else if (seats[row][seat] == EMPTY) {
+	//			rowsWanted.push_back(row);
+	//			seatsWanted.push_back(seat);
+	//			cout << row << ", " << seat << endl;
+	//			seats[row][seat] = TAKEN;
+	//			cout << "Another seat? (y/n): ";
+	//			cin >> anotherSeat;
+	//		}
+	//		else {
+	//			cout << "That seat is taken, try another seat." << endl;
+	//			row = 0;
+	//			seat = 'A';
+	//		}
+	//	}
+	//	displaySeatingChart(seats);
+	//	/*int totalCost = 0;
+	//	for (size_t i{ 0 }; i < rowsWanted.size(); ++i) {
+	//		totalCost += price(rowsWanted[i], seatsWanted[i]);
+	//	}
+	//	cout << "Total cost = " << totalCost << endl;*/
+	//}
+
+
+	//void Airline::displaySeatingChart(char seats[NUM_ROWS + 1][NUM_COLS + 1]) const {
+	//	cout << "\tSeats" << endl;
+	//	cout << "\t"
+	//		<< setw(2) << 'L'
+	//		<< setw(3) << 'R' << endl;
+	//	cout << "\t";
+	//	for (size_t i{ 1 }; i <= NUM_COLS; ++i) {
+	//		cout << i % 5;
+	//	}
+	//	cout << endl;
+
+	//	for (size_t i{ 1 }; i <= NUM_ROWS; ++i) {
+	//		cout << "Row " << i << "\t";
+	//		for (size_t j{ 1 }; j <= NUM_COLS; ++j) {
+	//			cout << seats[i][j];
+	//		}
+	//		cout << endl;
+
+	//	}
+	//}
+
+	//bool isValid(int row, int seat) {
+	//	return 1 <= row && row <= NUM_ROWS
+	//		&& 1 <= seat && seat <= NUM_COLS;
+	//}
 }
+
+	/*Airline::UserInfo(const std::string& firstName, const std::string& lastName,
+		const std::string& dateOfBirth, const std::string& passportNo,
+		const std::string& gender, const std::string& nationality) : mFirstName(firstName), mLastName(lastName),
+		mDateOfBirth(dateOfBirth), mPassportNo(passportNo), mGender(gender), mNationality(nationality) {
+
+	}*/
+
+	/*const std::string& getPassportExpiration() const;
+	void setPassportExpiration()*/;
+
+	/*void Airline::userTicketInformation(const std::string& firstName, const std::string& lastName) {
+
+	}
+	void Airline::userTicketInformation(int AirlineNumber, const std::string& departureLocation,
+		const std::string& depatureDate, const std::string& departureTime,
+		const std::string& arrivalLocation, const std::string& arrivalDate,
+		const std::string& arrivalTime) {
+
+	}*/
+
--- AirlineReservationSystem/AirlineDB.h
+++ AirlineReservationSystem/AirlineDB.h
@@ -2,23 +2,42 @@
 #include <iostream>
 #include <vector>
 #include "Airline.h"
-#include "UserInfo.h"
 
 namespace AirlineReservationSystem {
 	const int kAirlineNumber = 200;
+	const int kFirstBookingNumber = 1000;
 	const int kSeatNumber = 1;
+	
 
-	class AirlineDatabase {
+	class AirlineDB {
 	public:
-		Airline& addAirline(int airlineNo);
 		Airline& addAirline(const std::string& departureLocation, const std::string& departureDate,
 			const std::string& departureTime, const std::string& arrivalLocation,
 			const std::string& arrivalDate, const std::string& arrivalTime);
+		Airline& getAirline(int airlineNO);
 		Airline& getAirline(const std::string& departureLocation, const std::string& departureDate,
 			const std::string& departureTime, const std::string& arrivalLocation,
 			const std::string& arrivalDate, const std::string& arrivalTime);
+		
+		Airline& addPassenger(const std::string& firstName, const std::string& lastName,
+			const std::string& dateOfBirth, const std::string& passportNo,
+			const std::string& gender, const std::string& nationality);
+		Airline& getPassenger(int bookingNo);
+		Airline& getPassenger(const std::string& firstName, const std::string& lastName,
+			const std::string& dateOfBirth, const std::string& passportNo,
+			const std::string& gender, const std::string& nationality);
+
+		void displayAll() const;
+
+		void displayAirlineSchedule() const;
+
+		void displayPassengerInfo() const;		
+		
+		
 	private:
-		std::vector<Airline> mAirline;
-		std::vector<Airline>
+		std::vector<Airline> mAirlines;
+		int mNextAirlineNumber = kAirlineNumber;
+		std::vector<Airline> mPassengers;
+		int mNextBookingNumber = kFirstBookingNumber;
 	};
 }
